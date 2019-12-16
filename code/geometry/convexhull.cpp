3d40b2b450cab669c0741b0f5e2e2b5661eb46fd
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2019-12-16 14:37:42 -0300

    Minor change at convex hull comparison
    to make it more floating-point friendly and add warning comment

--- code/geometry/convexhull.cpp
+++ code/geometry/convexhull.cpp
@@ -1,27 +1,23 @@
+// Be sure P has no repeated point if keeping all colinear points.
 vector<vec> monotone_chain_ch(vector<vec> P){
     sort(P.begin(), P.end());
 
     vector<vec> L, U;
     for(auto p : P){
-        while(L.size() >= 2 && L[L.size() - 2].cross(L.back(), p) < 0)
+        while(L.size() >= 2 && L[L.size() - 2].ccw(L.back(), p) < 0) // <= to remove colinear points
             L.pop_back();
-
         L.push_back(p);
     }
 
     reverse(P.begin(), P.end());
     for(auto p : P){
-        while(U.size() >= 2 && U[U.size() - 2].cross(U.back(), p) < 0)
+        while(U.size() >= 2 && U[U.size() - 2].ccw(U.back(), p) < 0)
             U.pop_back();
-
         U.push_back(p);
     }
 
     L.pop_back(), U.pop_back();
-
-    L.reserve(L.size() + U.size());
     L.insert(L.end(), U.begin(), U.end()); 
 
     return L;
 }
-
